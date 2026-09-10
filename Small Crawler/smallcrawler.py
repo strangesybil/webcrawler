9fbca51034ac0590041b8190bc233d4fc24dd617
Raemae Kok <raemaekok@gmail.com>
2026-09-10 12:35:05 -0400

    added comments and implemented priority q

--- Small Crawler/smallcrawler.py
+++ Small Crawler/smallcrawler.py
@@ -1,16 +1,19 @@
 import requests
+import threading
+import time
 from bs4 import BeautifulSoup
 from queue import PriorityQueue
 
 # Initialize a priority queue for the search results
 pq=PriorityQueue()
 
+#What is the user looking for? 
 search_query = input("What are you looking for?")
 
-seed_url = ["https://google.com/search",
-            "https://bing.com/search",
-            "https://duckduckgo.com/search"]
+#Where am I getting the results from? 
+seed_url = ["https://bing.com/search"]
 
+#Get search results from all the seed pages 
 for url in seed_url:
     response = requests.get(
         url,
@@ -19,16 +22,29 @@ for url in seed_url:
 
     print(response.status_code)
 
+#Download pages in html 
     with open(f"/Users/rae/Documents/source.html", "w",encoding="utf-8") as f:
         f.write(response.text)
 
+#Use BeautifulSoup library to parse response text 
     soup = BeautifulSoup(response.text, "html.parser")
 
     for link in soup.find_all("a",href=True):
         print(link["href"])
         url = link["href"]
-        pq.put(url)
-
+        if url.startswith("http"):
+            pq.put(url) #Add links to priority queue
+            print(f"\nHow many items are in the queue? {pq.qsize()}")
+        
+    while not pq.empty():
+        #Acquire and remove first item from the priority queue    
+        item=pq.get()
+        #Download pages in html
+        response=requests.get(item)
+        with open(f"/Users/rae/Documents/source1.html", "w",encoding="utf-8") as f:
+            f.write(response.text)
+        #Parse the response text using BeautifulSoup
+        soup = BeautifulSoup(response.text, "html.parser")
 
 
       
